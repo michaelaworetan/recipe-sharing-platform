37064a71e2768095b074f446bf4a4a15e2c026a5
Aworetan Michael <mvillacs07@gmail.com>
2024-07-08 04:13:19 +0100

    added recipeRoutes to app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 
 // Routes
 const authRoutes = require('./routes/authRoutes');
+const recipeRoutes = require('./routes/recipeRoutes');
 
 dotenv.config();
 
@@ -13,10 +14,12 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(express.json());
+app.use(express.json());   // Middleware to parse JSON bodies of incoming requests
+app.use(express.urlencoded({ extended: true }));    // Middleware to parse URL-encoded bodies of incoming requests
 
 //Routes
 app.use('/api/users', authRoutes);
+app.use('/api/recipes', recipeRoutes);
 
 // Root endpoint for health check
 app.get('/', (req, res) => {
